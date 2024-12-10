4bcd96bef96c88350d1b82d66be2b340f37ad764
rahul <shanrahul03@gmail.com>
2024-12-10 13:56:13 +0530

    new

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import BuyMeCoffee from './components/Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BuyMeCoffee/>
   </React.StrictMode>
 );
 
